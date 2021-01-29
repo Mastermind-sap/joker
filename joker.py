1ab758b869cc78bc36fcf81446b9ad58a09854ba
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2021-01-29 20:28:07 +0530

    Added akinator[w.i.p.]

--- joker.py
+++ joker.py
@@ -10,6 +10,7 @@ import googlesearch as gs
 import giphypop as gp
 import json
 from pytube import YouTube
+import akinator as ak
 
 token = open("token.txt", "r").read()
 mainaccid=open("mainaccid.txt", "r").read()
@@ -65,6 +66,7 @@ async def help(ctx):
     fun_embed.add_field(name="meme/memify",value="sends a random meme",inline=False)
     fun_embed.add_field(name="comic/xkcd",value="sends a random comic strip",inline=False)
     fun_embed.add_field(name="choice/choose/select/random",value="choose random item from a list",inline=False)
+    fun_embed.add_field(name="akinator/aki",value="Call the akinator",inline=False)
     await author.send(embed=fun_embed)
 
     #music commands
@@ -511,4 +513,43 @@ async def stop(ctx):
     voice.stop()
     await ctx.send("Stopped playing :octagonal_sign: ")
 
+
+
+
+
+@bot.command(aliases=["aki"])
+async def akinator(ctx):
+    await ctx.send("Akinator is here to guess!")
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel and msg.content.lower() in ["y", "n","p","b"]
+    try:
+        aki = ak.Akinator()
+        q = aki.start_game()
+        while aki.progression <= 80:
+            await ctx.send(q)
+            await ctx.send("Your answer:(y/n/p/b)")
+            msg = await bot.wait_for("message", check=check)
+            if msg.content.lower() == "b":
+                try:
+                    q=aki.back()
+                except ak.CantGoBackAnyFurther:
+                    await ctx.send(e)
+                    continue
+            else:
+                try:
+                    q = aki.answer(msg.content.lower())
+                except ak.InvalidAnswerError as e:
+                    await ctx.send(e)
+                    continue
+        aki.win()
+        await ctx.send(f"It's {aki.first_guess['name']} ({aki.first_guess['description']})! Was I correct?(y/n)\n{aki.first_guess['absolute_picture_path']}\n\t")
+        correct = await bot.wait_for("message", check=check)
+        if correct.content.lower() == "y":
+            await ctx.send("Yay\n")
+        else:
+            await ctx.send("Oof\n")
+    except Exception as e:
+        await ctx.send(e)
+
+        
 bot.run(token)
