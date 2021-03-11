ad97c9919f4b0db11e32b20d18b0dcd4e2232cc5
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2021-03-11 14:04:48 +0530

    Added chess commands
    Wait for sometime to be fully functional(prob with versions of packages)

--- joker.py
+++ joker.py
@@ -13,6 +13,7 @@ from pytube import YouTube
 import akinator as ak
 import asyncio
 import itertools
+import chessdotcom as chess
 
 token = open("token.txt", "r").read()
 mainaccid=open("mainaccid.txt", "r").read()
@@ -72,6 +73,10 @@ async def help(ctx):
     fun_embed.add_field(name="akinator/aki",value="Call the akinator",inline=False)
     fun_embed.add_field(name="mastermind",value="Play MASTERMIND -THE GUESSING GAME",inline=False)
     fun_embed.add_field(name="riddle",value="Generate a random riddle from a collection of 101 riddles")
+    fun_embed.add_field(name="chessprofile/get_player",value="Get player chess.com profile",inline=False)
+    fun_embed.add_field(name="leaderboards/print_leaderboards",value="Get chess leaderboards",inline=False)
+    fun_embed.add_field(name="stats/get_player_rating",value="Get player stats",inline=False)
+    fun_embed.add_field(name="chesspuzzle",value="Get random chess puzzle",inline=False)
     await author.send(embed=fun_embed)
 
     #music commands
@@ -762,5 +767,55 @@ async def msgserver(ctx):
                             await ctx.send(e)
                     
 
+@bot.command(aliases=["chessprofile"])
+async def get_player(ctx,*,username):
+    try:
+        data = chess.get_player_profile(username).json
+        data1=chess.is_player_online(username).json
+        data2=chess.get_player_clubs(username).json
+        profile=discord.Embed(title=data['username'],description=data['name'],color=discord.Colour.red())
+        profile.set_thumbnail(url=data['avatar'])
+        profile.add_field(name="Country:",value=data['country'],inline=False)
+        profile.add_field(name="Followers:",value=data['followers'],inline=False)
+        profile.add_field(name="Status:",value=data['status'],inline=False)
+        profile.add_field(name="Online:",value="Yes" if data1['online'] else "No",inline=False)
+        profile.add_field(name="Url:",value=data['url'],inline=False)
+        for i in data2['clubs']:
+            profile.add_field(name="Club:",value=i['name'],inline=False)
+        await ctx.send(embed=profile)
+    except Exception as e:
+        print(e)
+        await ctx.send("Username doesnot exist")
+
+@bot.command(aliases=["leaderboards"])
+async def print_leaderboards(ctx):
+        await ctx.send("Leaderboards")
+        data = chess.get_leaderboards().json
+        categories = data.keys()
+        for category in categories:
+                await ctx.send('Category:'+str(category))
+                for idx, entry in enumerate(data[category]):
+                    if idx<3:
+                        await ctx.send(f'Rank: {idx + 1} | Username: {entry["username"]} | Rating: {entry["score"]}')
+                    else:
+                        break
+@bot.command(aliases=["stats"])
+async def get_player_rating(ctx,*,username):
+        await ctx.send("Player Ratings")
+        data = chess.get_player_stats(username).json
+        categories = ['chess_blitz', 'chess_rapid', 'chess_bullet']
+        for category in categories:
+        	await ctx.send('Category:'+str(category))
+        	await ctx.send(f'Current: {data[category]["last"]["rating"]}')
+        	await ctx.send(f'Best: {data[category]["best"]["rating"]}')
+        	await ctx.send(f'Record: {data[category]["record"]}')
+
+@bot.command()
+async def chesspuzzle(ctx):
+        await ctx.send("Random Puzzle")
+        data = chess.get_random_daily_puzzle().json
+        await ctx.send(data['title'])
+        await ctx.send(data['image'])
+        await ctx.send(data['url'])
         
 bot.run(token)
